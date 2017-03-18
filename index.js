b39e79abda6dc62b904aeb300f0edf22af7f9e6c
Cole Furfaro-Strode <colestrode@gmail.com>
2017-03-18 16:35:08 -0400

    renames plugin to order

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 require('skellington')({
   slackToken: process.env.SLACK_TOKEN,
-  plugins: [require('./plugins/welcome'), require('./plugins/pizza')]
+  plugins: [require('./plugins/welcome'), require('./plugins/order')]
 })
